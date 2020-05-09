b68fba7bd0c5d53749467f6e390f08965843138d
SyluxDX <syluxdx@gmail.com>
2020-05-09 14:08:20 +0100

    working state

--- README.md
+++ README.md
@@ -3,10 +3,7 @@
 ![upload layout](UYsFAHP.jpeg)
 
 ### Todo
-- [ ] Get file parser/checker
-- [x] Learn/use rendering html template
-- [ ] Generate confirmation page
-- [ ] Accept confirmation (using forms) and do something to file
+- [ ] Move configurations to Json file
 #### Optional
 - [ ] Create connection to mongodb
 - [ ] Upsert (or clear, insert) data file to mongo
--- main.go
+++ main.go
@@ -8,17 +8,21 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"path/filepath"
 )
 
 type simpleMsg struct {
 	Code int    `json:"Code"`
 	Msg  string `json:"Msg"`
 }
+type confimation struct {
+	Name  string
+	Lines []string
+}
 
 func uploadFile(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("upload Hit")
 	r.ParseMultipartForm(32 << 20)
-	file, handler, err := r.FormFile("myFile")
+	file, _, err := r.FormFile("myFile")
 	if err != nil {
 		log.Println(err)
 		w.Header().Add("Content-Type", "application/json")
@@ -41,43 +45,45 @@ func uploadFile(w http.ResponseWriter, r *http.Request) {
 	// write this byte array to our temporary file
 	tempFile.Write(fileBytes)
 
-	// send respose
-	w.Header().Add("Content-Type", "application/json")
-	w.WriteHeader(200)
-	json.NewEncoder(w).Encode(simpleMsg{Code: 200, Msg: fmt.Sprintf("%v uploaded to %v", handler.Filename, tempFile.Name())})
-}
+	st := string(fileBytes)
+	fmt.Println(st)
 
-type todo struct {
-	Title  string
-	Phrase string
-	List   []string
-	Table  []string
-}
+	// request Confirmation page
+	confirm := confimation{tempFile.Name(), []string{}}
+	// check uploaded file
+	confirm.Lines = parseTesters(fileBytes)
 
-func homePage(w http.ResponseWriter, r *http.Request) {
-	lst := []string{"itqm", "asd", "qwe"}
-	tbl := []string{"line1", "liune 2 is basdmasd", "iulxcvlkqwe lkjzs", "qwezxcqawe"}
-	context := todo{"This is a title", "A paragraph is such a line of text", lst, tbl}
-	template, err := template.ParseFiles("test.html")
+	template, err := template.ParseFiles("confirm.html")
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 
-	if err := template.Execute(w, context); err != nil {
+	if err := template.Execute(w, confirm); err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
 }
 
-func staticUploadFile(w http.ResponseWriter, r *http.Request) {
-	http.ServeFile(w, r, "upload.html")
+func processFile(w http.ResponseWriter, r *http.Request) {
+	path := r.FormValue("filename")
+	filename := filepath.Base(path)
+	// move file to upload folder
+	os.Rename(path, filepath.Join("upload", filename))
+	// Serve final page
+	http.ServeFile(w, r, "thankyou.html")
 }
 
 func setupRoutes() {
-	http.HandleFunc("/", homePage)
-	http.HandleFunc("/testupload", staticUploadFile)
-	http.HandleFunc("/upload", uploadFile)
-	http.ListenAndServe(":8080", nil)
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "upload.html")
+	})
+	http.HandleFunc("/confirm", uploadFile)
+	http.HandleFunc("/thankyou", processFile)
+
+	url := "127.0.0.1"
+	port := 8080
+	log.Printf("Serving on %[1]s port %[2]d (http://%[1]s:%[2]d/)\n", url, port)
+	http.ListenAndServe(fmt.Sprintf("%s:%d", url, port), nil)
 }
 
 func setupStuff() {
@@ -86,7 +92,7 @@ func setupStuff() {
 }
 
 func main() {
+	log.Println("Preping server")
 	setupStuff()
-	log.Println("starting server")
 	setupRoutes()
 }
